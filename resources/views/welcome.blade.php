fd8a60ffbe093964329daa0f88ed36e6fa0b4b03
Trapence <trapencepempho@gmail.com>
2021-07-15 06:01:16 -0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>pempho</title>
+
+        
+        <style>
+            body {
+                font-family: 'Nunito', sans-serif;
+                <p style="text-align:center;">Learn from us</p>
+            }
+        </style>
+    </head>
+    <body class="antialiased">
+    <h1 style="text-align:center;"> Hello world!</h1>
+    </body>
+</html>
